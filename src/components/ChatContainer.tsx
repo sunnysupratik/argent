7074b98300c3507fe9399c9b3a7dc76e5ab24a6f
sunnysupratik <sunny.supratik@gmail.com>
2025-07-25 01:11:53 +0100

    Fix ChatContainer Default Export

---
+++ src/components/ChatContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ChatContainer: React.FC = () => {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-1 overflow-y-auto p-4">
+        <div className="text-center text-gray-500">
+          Chat functionality coming soon...
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatContainer;
